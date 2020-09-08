fd3d1a3230d7f2e8f6ea092ca42736bf9e215805
Sebastien Besson <seb.besson@gmail.com>
2020-09-08 11:28:26 +0100

    Make tile size, compression and resolution number configurable

--- convert.py
+++ convert.py
@@ -9,15 +9,15 @@ import sys
 log = logging.getLogger()
 
 
-def convert(inputfile, outputfile):
+def convert(inputfile, outputfile, tilesize=256, compression=6, resolutions=8):
     log.info("Reading %s" % inputfile)
     image = tifffile.imread(inputfile)
 
     print("Writing %s" % outputfile)
     with tifffile.TiffWriter(outputfile], bigtiff=True) as tif:
-      options = {'tile': (512, 512), 'compress': 6}
-      tif.save(image, subifds=8, **options)
-      for _ in range(8):
+      options = {'tile': (tilesize, tilesize), 'compress': compression}
+      tif.save(image, subifds=resolutions, **options)
+      for _ in range(resolutions):
         image = cv2.resize(
             image,
             (image.shape[0] // 2, image.shape[1] // 2),
@@ -29,6 +29,10 @@ def main(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument('inputfile', help='The file to convert')
     parser.add_argument('outputfile',help='The destination file')
+    parser.add_argument('tilesize', default=256, help='Tile size')
+    parser.add_argument('compression', default=6, help='Compression')
+    parser.add_argument(
+        'resolutions', default=8, help='Number of sub-resolutions')
     parser.add_argument(
         '--verbose', '-v', action='count', default=0,
         help='Increase the command verbosity')
@@ -39,7 +43,8 @@ def main(argv):
 
     logging.basicConfig(
             level=logging.INFO - 10 * args.verbose + 10 * args.quiet)
-    convert(args.inputfile, args.outputfile)
+    convert(args.inputfile, args.outputfile, tilesize=args.tilesize,
+            compression=args.compression, resolutions=args.resolutions)
 
 
 if __name__ == "__main__":
